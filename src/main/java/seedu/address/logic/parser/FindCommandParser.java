f943d212ed7db5f0cf472790d507e9b6299ba37a
Tan Zhang-Hng Ervin <58417787+MakiseKurisuX@users.noreply.github.com>
2025-03-20 22:10:41 +0800

    Update FindCommandParser.java

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -32,7 +32,7 @@ public class FindCommandParser implements Parser<FindCommand> {
     public FindCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, 
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL,
                     PREFIX_ADDRESS, PREFIX_POLICY, PREFIX_TAG);
 
         if (!argMultimap.getPreamble().isEmpty()) {
